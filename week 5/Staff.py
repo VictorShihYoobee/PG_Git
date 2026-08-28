9606e35e20a35c926bb9f3a9766099f1f3e2f17e
victor2vivi <victor2vivi@gmail.com>
2026-08-29 10:01:32 +1200

    W5 A3 activity

---
+++ week 5/Staff.py
@@ -0,0 +1,34 @@
+from Person import Person
+
+class Staff(Person):
+    def __init__(self, id, name, tax_num):
+        super().__init__(id, name)
+        self.tax_num = tax_num
+
+class General(Staff):
+    def __init__(self, id, name, tax_num, rate_of_pay):
+        super().__init__(id, name, tax_num)
+        self.rate = rate_of_pay
+    
+    def display_pay_rate(self):
+        print(f"General Staff: {self.name} (ID: {self.id})")
+        print(f"Pay Rate: ${self.rate:.2f}/hr\n")
+
+class Academic(Staff):
+    def __init__(self, id, name, tax_num, publications):
+        super().__init__(id, name, tax_num) 
+        self.publications = publications
+
+    def get_publications(self):
+        return len(self.publications)
+    
+if __name__ == "__main__":
+    #1. general
+    general = General(2,"gen", "111", 1000)
+    general.display_pay_rate()
+    
+    #2. academic
+    lecturer = Academic(1, "lect", "123", publications = ["p1", "p2"])
+    print("Numbers of publications: ", lecturer.get_publications())
+
+    
